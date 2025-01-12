0808ee24aead1301634017f0be472190cc9c12e9
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2025-01-12 12:26:58 +0300

    Add functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,7 +11,21 @@ testEpr expr res = do
     else
         print "Test passed."
 
+testFunc :: String -> [(String,Double)] -> Double -> IO ()
+testFunc expr m res = do
+    let parsed = parseExpression (pack expr :: Text)
+    let evaluated = evalFunction parsed m
+    if evaluated /= res then
+        die (expr ++ " should be equal to " ++ show res ++ " but its " ++ show evaluated)
+    else
+        print "Test passed."
+
 main :: IO ()
 main = do
+    testEpr "2.0" 2.0
+    testEpr "2.0 * 2.0" 4.0
+    testEpr "2.0 + 2.0" 4.0
     testEpr "2.0 * 2.0 + (1.0 * 3.5) ^ 2.0 - 2.0" 14.25
     testEpr "(1.0 + 3.2) * 3.8 ^ 3.0 / 3.4" 67.7830588235294
+    testFunc "x" [("x", 2.0)] 2.0
+    testFunc "x + y" [("x", 2.0), ("y", 4.0)] 6.0
